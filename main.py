6ffefc10b82bd3ca35af081a9d736239a516848b
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-30 16:45:33 +0000

    Assignment #5: Extra

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("Welcome! This Program will calculate tyhe circumference and area of a circle!")
 radius = float(input("Enter the radius of your circle"))
-circumference = (radius*2*3.14)
-area = (radius*radius*3.14)
+circumference = radius * 2 * 3.14
+area = radius * radius * 3.14
 circumferenceRounded = round(circumference, 2)
 areaRounded = round(area, 2)
 print("The circumference of your circle is", circumferenceRounded)
